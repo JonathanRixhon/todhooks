3ce0e630a1c6cb998e53a6d49ec9625c0aeb9249
JonathanRixhon <58252485+JonathanRixhon@users.noreply.github.com>
2020-11-17 11:05:13 +0100

    fist commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import Reat, { useState, Fragment } from 'react'
+function App() {
+	const [count, setcount] = useState(0)
+	return (
+		<Fragment>
+			<p>coucou</p>
+		</Fragment>
+	)
+}
+export default App
